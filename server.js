8b7b694a398572b6497b70d5ad81e9202cd7ab27
harkirat99 <harkirat2312@gmail.com>
2025-03-15 17:59:22 +0530

    endpoint created

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = 8000;
 
-app.get('/', (req, res) => {
+app.get('/api/get', (req, res) => {
     res.send('Hello, World from dashmesh api!');
 });
 
